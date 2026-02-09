205ff4a85dd2216c72d7fc903c524fef6d7a096f
Ashutosh Kesharwani <ashutoshkesharwani20021406@gmail.com>
2026-02-09 13:18:21 +0530

    Refactor server setup and middleware configuration in app.js and index.js

---
+++ src/app.js
@@ -0,0 +1,129 @@
+
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+const app = express();
+
+// for setuping up configuration for CORS
+
+// app.use(cors()); mostly issi trah se ham cors method ko define karte hai but ham iske jo potions milte hai usko bhi set kar sakte hai like 
+/*
+here we can add our object as a arg so yha 
+
+# here we are using our cors origin as universal as khi se hi aaye to bhi theek hai and but in production ham , koi specific url ka dete hai like vercel se aaye tbhi accept like that . 
+CORS_ORIGIN=* 
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    // ye origin batati hai ki hame kha se api acess karne  ki acess jo jai usse allow karna hai 
+
+
+    credentials:true,
+
+}))
+
+*/
+
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true,
+
+}))
+
+/*
+As jab hamare client side se request aati hai to kayi baar usme se data bhi aata hai , to wo data hamara json , form or diff type se aa skta hai to ham advance me settings configure kar lete hai ki hamara data kaise aaye , kis form me , and kitni limit .
+
+// ye  keh rha hai ki ham json ko except karenge and but uski size limmit 16bkb me ho max usse jada size ki nhi .
+// so phle hamara ye direct use nhi kar pate the uss smay hame body parser use karna padta hai
+app.use(express.json({
+    limit:"16kb",
+}))
+
+ye json except ke liye hame aaj kal direct express se mil jata hai 
+
+*/ 
+app.use(express.json({
+    limit:"16kb",
+}))
+
+
+/*
+Configure of url data as well : 
+as hamara data kayi baar url se bhi aata hai and alg alg site me alg type ka data like khi symbol enoceded , khi direct like ye sab so usse bhi cofigure karna padega 
+
+here as express directly provide us urlencoded method jha hame exteneded milta hai jisme ham ek obj me aur obj de sakten hai and limit bhi,
+
+ham ifm chahe agr limit har jgah constanr use ho rhi hai ek constant limit use karle har jgah and use ek env me store karle and jha chahe use karlo
+
+*/
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb",
+}))
+
+
+/*
+-> So yha mostly ham kya karte hai ki , jaise hamare client se koi data image, data , pdf aati hai to usse ham apne hi server me rkhna chahte hai to uske liye ham apne src me public filder banate hau usse iss trah sse batana padta hai , as if sever third party like aws , se connection loss ho to data temp yha store hom and show ho jaye .
+*/
+app.use(express.static("public"))
+
+/*
+Cookie configuration : ye use hota hai ki jisse jo mere client ki cookies hai unko mai use kar pau and usspe curd oper kar paye and server securely cookie ko acss kar paye .
+
+basically isse ham secure cookie bna sakte hai , and isse sirf server hi use kar paye only .
+
+app.use(cookieParser())
+mostly issme itene ki hi jarurate poadti hai option ki nhi 
+
+*/
+app.use(cookieParser())
+
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Jai Shree Ram | Har Har Mahadev </h1>')
+})
+
+
+export {app};
+
+/*Generally when we are using express so ham mainly to cheezo me focus karte hai api reference me , Request and response pe ,
+
+so here Request : Focus on ki kab kab kaise data aa rha  hai  hmare pass and ussse kaise handle karnege , 
+
+So here in req we can get various thing like req.params, req.ip , /.. so on
+
+
+Mainly we are going to use 2 :
+
+1) req.params as jda tar data hamare url me se aata hai so , ussi me se data lene/fetch ke liye 
+
+2) req.body -> Yha bhi hamara data alg alg tareeka se aa skta ha like form , json so usse bhi ham configure karenge , yha pe 
+
+3) req.cookies -> ham kayi baar data hamari cookies se bhi lete hai , so note for this we have to install cookie-parser pkg and middleware setup it.
+
+
+=> For handling cookie we need -> two things [cookie-parser , cors ]
+
+
+
+
+----
+
+Note whenever we are going to use middlewares , so we are generally going  to use app.use() , as get , post ye sab yha nhi use , 
+
+-> and app.use() ham tab use karte hai jab hame koi configuration setting karni hoti hai , ya jab middleware set karna ho tab .
+
+Response : me ki ham uss data ko kaise process karenge and wapis kaise send karenge cliet side pe .
+
+
+--------------------
+
+
+So these configuration for middleware and configuration setup pahle app.use use karo and then app.get sab
+*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,139 @@
-console.log("Jai maa Bhavani Ambe");
+import 'dotenv/config'
+import connectDB from './db/index.js'
+import {app} from './app.js'
+
+
+const port = process.env.PORT || 8000 ;
+
+
+
+// for env debugging , whether env loaded successfully or not 
+// console.log(process.env);
+
+
+/*
+Now as we are using async func in while connecting db , so we know that async func return a promise , so we can consume that promise as well with .then() and .catch(). and as we know afiter data base get successfully so now our time to listen to our server for that app ka use 
+*/
+
+
+connectDB()
+.then(()=>{
+
+// but before listening to our server we also want to listen for error , so here we are going to on listener for and use error event 
+
+app.on("error",(err)=>{
+    console.log(`ERROR : ${err}`);
+    throw err
+})
+
+// here listen as db connection ke baad , listen hi karna hota hai.
+app.listen(port,()=>{
+    console.log(`Server running at ${port}`);
+      console.log(`Server running at http://localhost:${port}`);    
+})
+})
+.catch((err)=>{
+  console.log(`MongoDB connection failed !! ${err}`);
+})
+
+
+
+
+
+
+/* 
+
+Note always try to import .env file as early as possible as issi file se ham jitne environment var hai uskla use karenge so isse generally ham hamare main enrty point me import karate hai , at top as isme ho gya to khi pe bhi use kar sakte hai as vo at end yhi entry level pe hi call hoga .
+-> import dotenv from 'dotenv';
+-> abb ye chalega hi nhi isse config bhi karana 
+padega so [ye hmara ek obj lega jisme path bhi daalo ki kha .env file hai and now abb ye work karne lagega har jgah , ]
+ dotenv.config({
+  path:'./env'
+})
+To debug whether env file get load or not 
+
+console.log(process.env);
+
+🚀 console.log(process.env) — What It Does
+
+It prints all environment variables available to your Node process.
+-----------------------------------
+
+import mongoose from 'mongoose';
+import {DB_NAME} from './constants.js';
+
+import express from  'express';
+import 'dotenv/config'
+
+const PORT = process.env.PORT || 3000;
+This way also going to work IIFE is better as vo banne ke baad turant invoke ho jata hai , and usko asynchronous bnane ke liye simply usme async likhdo
+like this [
+;(async ()=>{})();] and most of people apply semicolon before iife as maan lo phle wale code me kisi ne semi colon nhi lagaya to safety ke liye fallback ke case me 
+
+function connectDB(){
+}
+
+connectDB()
+
+->Mostly jab log index.js me hamara db connect karte hai to vo kya karte hai  iss trah ka syntax follow karte hai 
+
+so here in async call nack  use error handling as , isme error bhi aa skta hai . use try and catch
+
+>so here always write db connection after the await , as ye time taking process hai , so isme smay bhi lag skta hai.
+
+2) here most of people ye kaam karte hai ki jaisi hi db connect hua uske baad listeners laga dete hai 
+here like app.on("event",callbackFN) , here event me ham event type rakhte hai ,like error etc.
+
+3)app.listen() yha pe hame , ehi bta hai kki kuj se port pe listen kar rahe hai ...
+
+4)   throw error;ye line use as when error aaye to usse throw karke hamare program se exit kar jao , so uske liye ham throw use karte hai ,
+otherways are like [use : process.exit(1) -> ye hmaara node js ek process name ka keyword provide karata haijo hamare current process ka reference hota hai , to isko use karke bhi we can exit, 
+and isse hame import bhi nhi karna padta hai ye node ka inbuilt hai  ]
+
+5) we can also store our await i.e connection wali line ko kisi variable me and ham isse print kara ke bhi dhkeho kya kya info me milti hai hame isse .
+like this [const connectionInstance =    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);] and hame iss instance me ek host milta hai jo btatta hai ki kha pe hmara kis url me db connect ho haa rha hai ,as produc me ya khi aur
+
+console.log(`\n MongoDB connected !! DB Host : ${connectionInstance}`);
+mainly used for checking ki ham kha pe connect ho rahe hai , production ki kisis aur state server link pe 
+
+
+6) Last step as hamnr connection kar diya ti usse import kara ke uss fiunc ko ya call kardo
+import connectDB from "./db";
+
+connectDB();
+
+
+
+
+-----------------------------
+Code for if write in Index.js main file  :
+
+[everything dbConnection , express for listener]
+
+(async ()=>{
+    try{
+
+      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+      app.on("error",(error)=>{
+        console.log(`ERROR : ${error}`);
+        throw error;
+      })
+      app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)});
+
+
+
+    }catch(error){
+        console.log(`ERROR : ${error}`);
+        throw error;
+    }
+})();
+
+Problem : In this approach -> is that , as even though here we write everything in oir index.js file , byut is not a good approach to pollute our main entry level file of App.
+
+So we always try to make our code modular and write separate task in separate folder like express part in app.js and db connection in db directory [ke index.js me ] we use files and wha pe func bna ke export kare and yha pe import kar le , fir simply index.hjs me usko, call import karke use karle .
+
+*/
+
+
